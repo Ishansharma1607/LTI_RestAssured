448b69adfde4c5fd1d59763b0224ad355f6913dd
Ishan <ishansharma1607@gmail.com>
2021-10-12 14:54:21 +0530

    update LTI_ResrAssured

--- LTI_RestAssured/src/test/java/day2/GitHub.java
+++ LTI_RestAssured/src/test/java/day2/GitHub.java
@@ -1,21 +1,65 @@
-package day2;
+//https://api.github.com/user/repos
 
-import org.testng.annotations.Test;
+
+package day2;
 
 import static io.restassured.RestAssured.*;
 
+import org.json.simple.JSONObject;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+
 public class GitHub {
-  @Test
-  public void gettingAllRepoitories() {
+	
+	
+	@BeforeTest
+	public void beforeTest() {
+		baseURI= "https://api.github.com/user/repos";
+		authentication= oauth2("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");
+		
+		
+	}
+	
+	 @Test(enabled=false)
+	  public void gettingAllRepositories() {
+		   get()
+	.then()
+		  .log()
+		  .body()
+		  .statusCode(200);
+	  }
+	
+	
+  @Test(enabled=true)
+  public void createRepositories() {
+	  
+	  JSONObject data = new JSONObject();
+	
+		
+		data.put("name", "RestAssuredCreations3");
+		data.put("description", "I am Created By RestAssured Tool");
+		data.put("homepage", "https://github.com/Ishansharma1607");
 	  
 	given()
-			.auth()
-			.oauth2("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")
-			.when()
-			.get("https://api.github.com/user/repos")
-		.then()
+//			.auth()
+//			.oauth2("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")
+			.header("Content-Type","application/json")
+			.body(data.toJSONString())
+	//System.out.println(data.toJSONString());
+	.when()
+			.post()
+	.then()
 		.log()
 		.body()
-		.statusCode(200);
+		.statusCode(201);
+			
+		//.time(Matchers.lessThan(2000L),TimeUnit.MILLISECONDS);
   }
+
+
+
+ 
+	 
 }
